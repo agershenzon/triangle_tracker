fa74a24b94c6fb918283e7b3c99e5f68ee1ac51f
Aaron Gershenzon and Eddie Duro <e.jamesduro@gmail.com>
2016-01-19 12:51:21 -0800

    refactor tirangleTracker functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,45 @@
 function triangleTracker(side1, side2, side3){
-  return ((side1 === side2) && (side2 === side3) || (side1 === side2) && (side2 !== side3) || (side1 !== side2) && (side2 !== side3) || (side1 + side2 <= side3) && (side1 + side3 <= side2) && (side2 + side3 <= side1));
+
+  if ((side1 + side2 <= side3) ||(side2 + side3 < side1) || (side3 + side1 < side2)) {
+    alert('not a triangle');
+    return true;
+  } else if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+    alert('Enter in a value larger than 0');
+    return true;
+  } else if (side1 === side2 && side2 ===side3) {
+    alert('equilateral');
+    return true;
+  } else if ((side1 === side2)||(side2 === side3)||(side3 === side1)) {
+    alert('isoceles');
+    return true;
+  } else if ((side1 !== side2)&&(side2 !== side3)&&(side3 !== side1)) {
+    alert('scalene');
+    return true;
+  }
 }
+
+$ (document).ready(function(){
+  $("form#triangle-tracker").submit(function(event){
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var results = triangleTracker(side1, side2, side3);
+
+    event.preventDefault();
+  });
+});
+
+
+// if ((side1 === side2) && (side2 === side3) && (side1 !== 0)) {
+//   alert("equilateral");
+//   return true;
+// } else if (((side1 === side2) && (side2 !== side3) || (side2 === side3) && (side1 !== side3) || (side1 === side3) && (side3 !== side2)) && (side1, side2, side3 !== 0)) {
+//   alert("isoceles");
+//   return true;
+// } else if (((side1 + side2 > side3) && (side1 !== side2) || (side2 + side3 > side1) && (side2 !== side3) || (side1 + side3 > side2) && (side1 !== side3) && ((side1 !== 0) && (side2 !== 0) && (side3 !== 0)))) {
+// alert("scalene");
+// return true;
+// } else if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)) {
+//   alert("try again");
+//   return true;
+// }
