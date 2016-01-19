b7924e3398b6ba0bf43ceabd7c5237fdd99f2932
Aaron Gershenzon and Eddie Duro <aaron@urbankayaks.com>
2016-01-19 10:35:34 -0800

    refactor triangleTracker functionality into single line return

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,16 @@ describe('triangleTracker', function() {
   it("will take in 3 value that are all equal", function() {
     expect(triangleTracker(3, 3, 3)).to.equal(true);
   });
+
+  it("will take in 2 value that are equal but not equal to the 3rd side", function(){
+    expect(triangleTracker(2,2,3)).to.equal(true);
+  });
+
+  it("will take in 3 values that are not equal", function() {
+    expect(triangleTracker(2,3,4)).to.equal(true);
+  });
+
+  it("take the sum of two sides is less than or equal to the 3rd side", function(){
+    expect(triangleTracker(2,2,5)).to.equal(true);
+  })
 });
